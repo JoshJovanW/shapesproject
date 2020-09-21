c9e1d0cefcbb33271885a0e1af783ee6a3b0b18b
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2020-09-21 21:41:39 +0700

    Update manual_BINGO.py

--- manual_BINGO.py
+++ manual_BINGO.py
@@ -1,34 +1,83 @@
-a = [[1,1,1], [1,2,1], [1,2,1]]
-if a[0][0] == a[1][0] and a[1][0] == a[2][0]:
-    print("BINGO!")#to the right from top
-else:
-    print("not bingo")
+a = [[1, 1, 1], [1, 2, 1], [1, 2, 1]]
 
-if a[0][0] == a[1][1] and a[1][1] == a[2][2]:
-    print("BINGO!")#diagonal to bottom right
-else:
-    print("not bingo")
+def Top_Right_From_Top():
+    if a[0][0] == a[0][1] and a[0][1] == a[0][2]:
+        return True
+
+    # to the right from top
 
 
-if a[0][0] == a[0][1] and a[0][1] == a[0][2]:
-    print("BINGO!")#to the bottom left from top
-else:
-    print("not bingo")
+def Diagonal_To_Bottom_Right():
+    if a[0][0] == a[1][1] and a[1][1] == a[2][2]:
+        return True
+    # diagonal to bottom right
 
 
-if a[0][1] == a[1][1] and a[1][1] == a[2][1]:
-    print("BINGO!")#to the bottom middle from the middle top
-else:
-    print("not bingo")
+def Bottom_Left_From_Top():
+    if a[0][0] == a[1][0] and a[1][0] == a[2][0]:
+        return True
+    # to the bottom left from top
 
 
-if a[0][2] == a[1][2] and a[1][2] == a[2][2]:
-    print("BINGO!")#bottom right from top right
-else:
-    print("not bingo")
+def Bottom_Middle_From_Mid_Top():
+    if a[0][1] == a[1][1] and a[1][1] == a[2][1]:
+        return True
+    # to the bottom middle from the middle top
 
 
-if a[0][1] == a[1][1] and a[1][1] == a[2][1]:
-    print("BINGO!")#middle right from middle left
-else:
-    print("not bingo")
+def Bottom_Right_From_Top_Right():
+    if a[0][2] == a[1][2] and a[1][2] == a[2][2]:
+        return True
+    # bottom right from top right
+
+
+def Middle_Left_From_Right():
+    a[1][0] == a[1][1] and a[1][1] == a[1][2]
+    # middle left from middle right
+
+
+def Bottom_Left_To_Bottom_Right():
+    a[2][0] == a[2][1] and a[2][1] == a[2][2]
+    # bottom left to bottom right
+
+
+def Bottom_Left_From_Top():
+    a[2][0] == a[1][1] and a[1][1] == a[0][2]
+    # bottom left to top right
+
+
+def Last_Resort():
+    print("NOT BINGO!")
+
+
+def Bingo():
+    print("BINGO!")
+
+
+if Top_Right_From_Top() == True:
+    Bingo()
+
+elif Diagonal_To_Bottom_Right() == True:
+    Bingo()
+
+elif Bottom_Left_From_Top() == True:
+    Bingo()
+
+elif Bottom_Middle_From_Mid_Top() == True:
+    Bingo()
+
+elif Bottom_Right_From_Top_Right() == True:
+    Bingo()
+
+elif Middle_Left_From_Right() == True:
+    Bingo()
+
+elif Bottom_Left_To_Bottom_Right() == True:
+    Bingo()
+
+elif Bottom_Left_From_Top() == True:
+    Bingo()
+
+else: 
+    Last_Resort()
+
